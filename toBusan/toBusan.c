1e3f4f19f760115ab76baeeddf1c76207c6f6eb9
yunmi <yunmi5728@gmail.com>
2024-05-27 00:47:18 +0900

    마동석 행동, 이동 추가, 어그로 추가,

--- toBusan/toBusan.c
+++ toBusan/toBusan.c
@@ -22,9 +22,9 @@
 #define ACTION_PROVOKE 1
 #define ACTION_PULL 2
 
-int len, prob, stm, c, m, z;
+int len, prob, stm, c, m, z, c_aggro, m_aggro;
 int turn = 1;
-int a, b;
+int c_prob,z_prob, m_prob, m_act;
 void intro_len(void) {
 	while (1) {
 		printf("train length(15~50)>>");
@@ -49,45 +49,21 @@ void intro_stm(void) {
 	}
 	m = len - 2;
 }
-void intro_train(void) {
-	if (len >= 15 && len <= 50) {
-		for (int i = 0; i < len; i++) {
-			printf("#");
-		}
-		printf("\n");//기차 길이 출력
-		for (int i = 1; i <= len; i++) {
-			if (i == 1 || i == len) {
-				printf("#");
-			}
-			else if (i == len - 2) {
-				printf("M");//마동석 출력
-			}
-			else if (i == len - 3) {
-				printf("Z");//좀비 출력
-			}
-			else if (i == len - 6) {
-				printf("C");// 시민 출력
-			}
-			else {
-				printf(" ");
-			}
-		}
-		printf("\n");
-		for (int i = 0; i < len; i++) {
-			printf("#");
-		}
-	}
-}
-void move(void) {
+void cz_move(void) {
 	c = len - 6;
 	z = len - 3;
-	a = rand() % 100;
-	if (a <= 100 - prob) {
+	c_aggro = 0;
+	c_prob = rand() % 100;
+	if (c_prob <= 100 - prob) {
 		--c;// 시민 움직이고 위치 저장
+		++c_aggro;
 	}
-	b = rand() % 100;
+	else {
+		--c_aggro;
+	}
+	z_prob = rand() % 100;
 	if (turn % 2 == 1) {
-		if (b <= prob) {
+		if (z_prob <= prob) {
 			--z;//좀비 움직이고 위치 저장
 		}
 	}
@@ -123,15 +99,15 @@ void train(void) {
 	}
 	printf("\n");
 }
-void print_move(void) {
-	if (a <= 100 - prob) {
-		printf("citizen = %d -> %d\n", c + 1, c);
+void print_move_cz(void) {
+	if (c_prob <= 100 - prob) {
+		printf("citizen = %d -> %d(agrro: %d)\n", c + 1, c, c_aggro);
 	}
 	else {
 		printf("citizen = stay  %d\n", c);
 	}
 	if (turn % 2 == 1) {
-		if (b <= prob) {
+		if (z_prob <= prob) {
 			printf("zombie = %d -> %d\n", z + 1, z);
 		}
 		else {
@@ -144,13 +120,54 @@ void print_move(void) {
 	printf("\n");
 	++turn;
 }
+void m_move(void){
+	m = len - 2;
+	while(1){
+		printf("madongseok move(0:stay, 1:left)>>"); 
+		scanf_s("%d", &m_prob);
+		if (m_prob == 0||m_prob==1) { break; }
+	}
+	if (m_prob == 1) {
+		--m;
+		++m_aggro;
+	}
+	else { --m_aggro; }
+}
+void m_action(void) {
+	priintf("madongseok action(0.rest, 1.provoke)>>");
+	scanf_s("%d", &m_act);
+	if (m_act == 0) { 
+		printf("madongseok rest...\n");
+		--m_aggro;
+		++stm;
+		printf("madongseok %d (aggro: %d -> %d, stamina: %d ->%d", m_aggro + 1, m_aggro, stm - 1, stm);
+	}
+}
+void print_m_move(void) {
+	if (m_prob == 0) {
+		printf("madongseok: stay %d(aggro: %d -> %d, stamina: %d", m, m_aggro,m_aggro + 1, stm);
+	}
+}
+void action(void) {
+	if (c < z) { printf("citizen does nothing.");	}
+	if (z > c || z < m) {	printf("zombie attcked nobody.\n");	}
+	else if (z == c + 1) {printf ("GAME OVER!citizen dead..."); }
+
+}
+void win(void) {
+	if (c == 1) {
+		printf(" YOU WIN!");
+	}
+}
 int main(void) {
 	intro_len();
 	intro_prob();
 	intro_stm();
+	train();//초기 기차
+	cz_move();
+	train();//움직인 기차1
+	print_move_cz();
+	m_move();
 	train();
-	move();
-	train();
-	print_move();
 	return 0;
 }
